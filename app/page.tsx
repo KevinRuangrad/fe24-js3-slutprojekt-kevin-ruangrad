2cc287ffb3138837c8e89c3389c66e05279383a6
Kevin Ruangrad <kevin_9@live.se>
2025-10-17 09:10:19 +0200

    UI fix

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Input } from "@/components/ui/input";
+
+export default function Home() {
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center min-h-screen gap-16">
+                <h1 className="text-8xl font-sans">Travel App</h1>
+                <Input className="w-2xl" placeholder="Search for country..." />
+            </div>
+        </>
+    );
+}
